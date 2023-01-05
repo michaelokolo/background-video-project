673948634c57aa84fd34deee89f16666cb13fd25
Michael Okolo <michaelokolo62@yahoo.com>
2023-01-04 22:36:17 -0500

    Added a preloader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.btn');
 const video = document.querySelector('.background-video');
 const fa = document.querySelector('.fa');
+const preloader = document.querySelector('.preloader');
 
 btn.addEventListener('click', ()=>{
     if(btn.classList.contains('pause')){
@@ -14,4 +15,8 @@ btn.addEventListener('click', ()=>{
         fa.classList.add('fa-pause');
         fa.classList.remove('fa-play')
     }
+});
+
+window.addEventListener('load', ()=>{
+    preloader.style.zIndex = '-2';
 })
